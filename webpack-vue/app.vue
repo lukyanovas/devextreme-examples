c35f8620546fea25c67ce3a95ccdd7248598c9e3
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-05-16 13:51:34 +0300

    Add PivotGrid Overview

--- webpack-vue/app.vue
+++ webpack-vue/app.vue
@@ -1,14 +1,19 @@
 <template>
-    <data-grid-overview />
+    <div>
+        <data-grid-overview />
+        <pivot-grid-overview />
+    </div>
 </template>
 
 <script>
 
 import DataGridOverview from "./components/data-grid-overview";
+import PivotGridOverview from "./components/pivot-grid-overview";
 
 export default {
     components: {
-        DataGridOverview
+        DataGridOverview,
+        PivotGridOverview
     },
     name: "app"
 }
