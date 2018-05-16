9b5599779269eec6ab5bf2b39c8d0269c078f2b1
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-05-16 15:01:08 +0300

    Add TreeList Overview

--- webpack-vue/app.vue
+++ webpack-vue/app.vue
@@ -2,6 +2,7 @@
     <div>
         <data-grid-overview />
         <pivot-grid-overview />
+        <tree-list-overview />
     </div>
 </template>
 
@@ -9,11 +10,13 @@
 
 import DataGridOverview from "./components/data-grid-overview";
 import PivotGridOverview from "./components/pivot-grid-overview";
+import TreeListOverview from "./components/tree-list-overview";
 
 export default {
     components: {
         DataGridOverview,
-        PivotGridOverview
+        PivotGridOverview,
+        TreeListOverview
     },
     name: "app"
 }
